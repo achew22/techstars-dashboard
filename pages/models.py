ce86ae79642d48a08f2f79cbc8f651ec09b4a2b9
Andrew Allen <achew22@gmail.com>
2011-07-17 20:59:14 -0600

    Add string helper for Page model

--- pages/models.py
+++ pages/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Page(models.Model):
 	url = models.URLField()
 	timeout = models.IntegerField()
+	
+	def __unicode__(self):
+	    return "Page (for %s sec): %s" % (self.timeout, self.url)
